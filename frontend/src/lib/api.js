4aaa6453adbc3d9c246524530c2bcc5729ea6d95
ChivaMihai <gheghe496@gmail.com>
2026-07-28 23:10:03 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,6 +4,13 @@ export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({ baseURL: API, withCredentials: true });
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export const resolveImg = (url) => {
   if (!url) return "";
